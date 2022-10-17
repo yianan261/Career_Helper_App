55b177ce7d331e74d2162b48896c8c112bf4b5ba
yianan261 <yian.chen0131@gmail.com>
2022-10-17 00:45:46 -0700

    yian commit

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
-import express from "express";
-import dotenv from "dotenv";
-import bodyParser from "body-parser";
-// import connectDB from "./config/db";
+const express = require("express");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const tasks = require("./routes/tasks");
+const { MongoClient } = require("mongodb");
 
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use("/sign-up",(req,res,next)=>{
+//     res.send()
+// })
+
+app.use(express.static("./public"));
+
+app.use("/tasks", tasks);
+
+// app.get("/sign-up.html", (req, res) => {
+//   res.send()
+// });
+
 app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+//connecting mongoDB
+const client = new MongoClient(process.env.MONGO_URI);
+const run = async () => {
+  try {
+    await client.connect();
+    console.log("connected success");
+  } finally {
+    await client.close();
+  }
+};
+
+run().catch(console.dir);
---
+++ controllers/tasks.js
@@ -0,0 +1,8 @@
+const getTasks = (req, res) =>{
+    res.status(200).json({tasks:"success"})
+}
+
+const getTasksId = (req, res) =>{
+    res.status(200).json({tasks:req.params})
+}
+module.exports = {getTasks, getTasksId}
