d0ce3ff18bd89e1fbdbe8ef6d42c11df71c8ac54
amandaay <amandaay.aysma@gmail.com>
2022-10-29 17:09:45 -0700

    added authentication for tracker: still needs edit

--- public/js/my-tracker.js
+++ public/js/my-tracker.js
@@ -1,10 +1,4 @@
-// declare api in 'routes'(nodejs)
-// implement the "api" method in controllers(nodejs)
-// insert "form data" to mongodb based the "api"(vanilla js)
-// declare api1 in 'routes'(nodejs)
-// implement the "api1": this "api1" is to get all the form data from mongodb(nodejs)
-// call "api1" in vanillasJS, in print it out
-
+// So Man Amanda Au-Yeung
 function Index() {
   const index = {};
 
@@ -42,8 +36,6 @@ function Index() {
       updatesDiv.appendChild(uDiv);
     }
   }
-
-  // if session.user -- authenticate
   
   function getTracker() {
     const form = document.getElementById("tracker-form");
@@ -56,6 +48,8 @@ function Index() {
     }
   }
 
+  // if session.user -- authenticate
+  
   async function createTracker(form) {
     try {
       await fetch("./tracker", {
--- routes/tracker.js
+++ routes/tracker.js
@@ -1,3 +1,4 @@
+// So Man Amanda Au-Yeung
 import express from "express";
 let router = express.Router();
 import myDB from "../db/myDB.js";
@@ -5,17 +6,30 @@ import myDB from "../db/myDB.js";
 router.post("/", async (req, res) => {
   console.log("params", req.body);
   const user = req.session.user;
-  const new_company = await myDB.createTracker(req.body, user.user);
-  console.log("INSERT BODY", new_company);
-  res.json(new_company);
+  if (!req.session.user) {
+    return res.json({
+      isLoggedIn: false,
+      err: "Please log in!",
+    });
+  } else {
+    const new_company = await myDB.createTracker(req.body, user.user);
+    console.log("INSERT BODY", new_company);
+    res.json(new_company);
+  }
 });
 
 router.get("/", async (req, res) => {
   console.log("params of get all trackers", req.body);
   const user = req.session.user;
-  const result = await myDB.getAllTracker(user.user);
-  res.status(200).json({result});
+  if (!req.session.user) {
+    return res.json({
+      isLoggedIn: false,
+      err: "Please log in!",
+    });
+  } else {
+    const result = await myDB.getAllTracker(user.user);
+    res.status(200).json({ result });
+  }
 });
-
 
 export default router;
