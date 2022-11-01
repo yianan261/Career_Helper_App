88f7d80665f1f105bcb37815db2d46fc22187d28
yianan261 <yian.chen0131@gmail.com>
2022-11-01 00:14:51 -0700

    deleted unwanted file

--- controllers/tasks.js
+++
@@ -1,7 +0,0 @@
-export const getTasks = (req, res) => {
-  res.status(200).json({ tasks: "success" });
-};
-
-export const getTasksId = (req, res) => {
-  res.status(200).json({ tasks: req.params });
-};
